6413925f455fe48a59e8dfbe5b1e7a8feb0f1224
Titus Onzere <titusonzere2@gmail.com>
2025-01-22 10:41:48 +0300

    Creaed a txtx file to hold all things I have installed

--- backend/mycontazapp/serializers.py
+++ backend/mycontazapp/serializers.py
@@ -1,26 +1,46 @@
-from Rest_Frameworks import serializers
-from .models import SubscriptionPlan, CustomUser, UseProfile, Category
+from rest_framework import serializers
+from .models import SubscriptionPlan, CustomUser, UseProfile, Category, Contact
+
 
 # Creating serializers
 class SubscriptionSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = SubscriptionPlan
-    fields = ['id' ,'name', 'max_contacts']
+    class Meta:
+        model = SubscriptionPlan
+        fields = ["id", "name", "max_contacts"]
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = CustomUser
-    fields = ['id', 'first_name', 'last_name', 'email', 'password']
+    class Meta:
+        model = CustomUser
+        fields = ["id", "first_name", "last_name", "email", "password"]
 
 
 class UseProfileSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = UseProfile
-    fields = ['id', 'user' , 'profile']
+    class Meta:
+        model = UseProfile
+        fields = ["id", "user", "profile"]
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "gender",
+            "date_added",
+            "birthday",
+            "address",
+            "image",
+            "category",
+            "owner",
+        ]
 
 
 class CategorySerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Category
-    fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'gender', 'date_added', 'birthday', 'address', 'image', 'category', 'owner']
+    class Meta:
+        model = Category
+        fields = ["category_name"]
